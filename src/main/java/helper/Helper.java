cdc3b8fd18de223034848de880e7a6e04a154ffe
ganjali <GAnjali@github.com>
2020-04-07 22:48:42 +0530

    Add functionalities to Helper with tests

--- src/main/java/helper/Helper.java
+++ src/main/java/helper/Helper.java
@@ -1,12 +1,19 @@
 package helper;
 
 import exceptions.InvalidTimeException;
+import model.Direction;
+import model.Vehicle;
+import reportGenerator.timeperiod.TimePeriod;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static constants.VehicleSurveyAnalyserConstants.AVERAGE_WHEEL_BASE;
 
 public class Helper {
-    public int calculateDay(int milliseconds) {
-        return (int) Math.ceil(((milliseconds * 0.001) / 60 / 60) % 24);
+    public long getDay(int milliseconds) {
+        return TimeUnit.MILLISECONDS.toDays(milliseconds);
     }
 
     public int getTime(String record) throws InvalidTimeException {
@@ -17,8 +24,43 @@ public class Helper {
         }
     }
 
-    public float getSpeed(int time) {
-        float speed = (Float.parseFloat(AVERAGE_WHEEL_BASE) / time) * 3600;
+    public float getSpeed(int timeInMilliSeconds) {
+        long timeInSeconds = TimeUnit.MILLISECONDS.toSeconds(timeInMilliSeconds);
+        float speed = (Float.parseFloat(AVERAGE_WHEEL_BASE) / timeInSeconds) * 3600;
         return Float.parseFloat(String.format("%.2f", speed));
+    }
+
+    public long getTotalDays(List<Vehicle> vehicles) {
+        Vehicle lastVehicle = vehicles.get(vehicles.size() - 1);
+        return getDay(lastVehicle.getPassingTimeInMilliSeconds());
+    }
+
+    public List<Vehicle> getVehiclesByDay(long day, List<Vehicle> vehicles) {
+        return vehicles.stream().filter(vehicle -> getDay(vehicle.getPassingTimeInMilliSeconds()) == day).collect(Collectors.toList());
+    }
+
+    public List<Vehicle> getVehiclesByTimePeriod(List<Vehicle> vehicles, TimePeriod timePeriod) {
+        if (timePeriod.equals(TimePeriod.MORNING))
+            return vehicles.stream().filter(this::isVehicleMovingInMorning).collect(Collectors.toList());
+        else
+            return vehicles.stream().filter(this::isVehicleMovingInEvening).collect(Collectors.toList());
+    }
+
+    private boolean isVehicleMovingInEvening(Vehicle vehicle) {
+        long vehiclePassingHour = getVehiclePassingHourInDay(vehicle);
+        return vehiclePassingHour > 18 && vehiclePassingHour <= 24;
+    }
+
+    private boolean isVehicleMovingInMorning(Vehicle vehicle) {
+        long vehiclePassingHour = getVehiclePassingHourInDay(vehicle);
+        return vehiclePassingHour <= 12 && vehiclePassingHour >= 0;
+    }
+
+    private long getVehiclePassingHourInDay(Vehicle vehicle) {
+        return TimeUnit.MILLISECONDS.toHours(vehicle.getPassingTimeInMilliSeconds()) % 24;
+    }
+
+    public int getVehicleCountByDirection(List<Vehicle> vehicles, Direction direction) {
+        return (int) vehicles.stream().filter(vehicle -> vehicle.getDirection().equals(direction)).count();
     }
 }
--- src/test/java/helper/HelperTest.java
+++ src/test/java/helper/HelperTest.java
@@ -1,32 +1,41 @@
 package helper;
 
 import exceptions.InvalidTimeException;
+import model.Direction;
+import model.Vehicle;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import reportGenerator.timeperiod.TimePeriod;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelperTest {
     private Helper helper;
+    private List<Vehicle> vehicles;
 
     @Before
     public void init() {
         helper = new Helper();
+        vehicles = new ArrayList<>();
     }
 
     @Test
     public void shouldReturnDay1ForGivenMilliseconds98186() {
-        Assert.assertEquals(1, helper.calculateDay(98186));
+        Assert.assertEquals(1, helper.getDay(90060911));
     }
 
     @Test
     public void shouldReturnDay2ForGivenMilliseconds5060911() {
-        Assert.assertEquals(2, helper.calculateDay(5060911));
+        Assert.assertEquals(2, helper.getDay(180060911));
     }
 
     @Test
     public void shouldReturnDayForGivenMilliseconds499718() {
-        Assert.assertEquals(18, helper.calculateDay(64356956));
+        Assert.assertEquals(7, helper.getDay(646356956));
     }
+
 
     @Test
     public void shouldReturnTimeForGivenRecordA98186() throws InvalidTimeException {
@@ -41,5 +50,77 @@ public class HelperTest {
     @Test(expected = InvalidTimeException.class)
     public void shouldReturnInvalidTimeExceptionForGivenRecord() throws InvalidTimeException {
         helper.getTime("A50.60911");
+    }
+
+    @Test
+    public void shouldReturnDays1ForGivenVehicleDataWith56060186MilliSeconds() {
+        vehicles.add(new Vehicle(Direction.NORTH, 99060186, 5));
+        Assert.assertEquals(1, helper.getTotalDays(vehicles));
+    }
+
+    @Test
+    public void shouldReturnDays6ForGivenVehicleDataWith560601863MilliSeconds() {
+        vehicles.add(new Vehicle(Direction.NORTH, 560601863, 5));
+        Assert.assertEquals(6, helper.getTotalDays(vehicles));
+    }
+
+    @Test
+    public void shouldReturnDays7ForGivenVehicleDataWith646356956MilliSeconds() {
+        vehicles.add(new Vehicle(Direction.NORTH, 646356956, 5));
+        Assert.assertEquals(7, helper.getTotalDays(vehicles));
+    }
+
+    @Test
+    public void shouldReturnListOfVehiclesForGivenVehicleDataWithDay1() {
+        vehicles.add(new Vehicle(Direction.NORTH, 99060186, 5));
+        vehicles.add(new Vehicle(Direction.NORTH, 560601863, 5));
+        vehicles.add(new Vehicle(Direction.NORTH, 646356956, 5));
+        List<Vehicle> vehiclesOnDay1 = helper.getVehiclesByDay(1, vehicles);
+        Assert.assertTrue(vehiclesOnDay1.contains(vehicles.get(0)));
+    }
+
+    @Test
+    public void shouldReturnListOfVehiclesForGivenVehicleDataWithDay7() {
+        vehicles.add(new Vehicle(Direction.NORTH, 99060186, 5));
+        vehicles.add(new Vehicle(Direction.NORTH, 560601863, 5));
+        vehicles.add(new Vehicle(Direction.NORTH, 646356956, 5));
+        List<Vehicle> vehiclesOnDay1 = helper.getVehiclesByDay(7, vehicles);
+        Assert.assertTrue(vehiclesOnDay1.contains(vehicles.get(2)));
+    }
+
+    @Test
+    public void shouldReturnListOfVehiclesForGivenVehicleDataWithTimePeriodMorning() {
+        vehicles.add(new Vehicle(Direction.NORTH, 99060186, 5));
+        vehicles.add(new Vehicle(Direction.NORTH, 560601863, 5));
+        vehicles.add(new Vehicle(Direction.NORTH, 776356956, 5));
+        List<Vehicle> vehiclesMovingInMorning = helper.getVehiclesByTimePeriod(vehicles, TimePeriod.MORNING);
+        Assert.assertTrue(vehiclesMovingInMorning.contains(vehicles.get(0)));
+    }
+
+    @Test
+    public void shouldReturnListOfVehiclesForGivenVehicleDataWithTimePeriodEvening() {
+        vehicles.add(new Vehicle(Direction.NORTH, 99060186, 5));
+        vehicles.add(new Vehicle(Direction.NORTH, 560601863, 5));
+        vehicles.add(new Vehicle(Direction.NORTH, 776356956, 5));
+        List<Vehicle> vehiclesMovingInMorning = helper.getVehiclesByTimePeriod(vehicles, TimePeriod.EVENING);
+        Assert.assertTrue(vehiclesMovingInMorning.contains(vehicles.get(2)));
+    }
+
+    @Test
+    public void shouldReturnCountOfVehiclesForGivenVehicleDataWithDirectionSouth() {
+        vehicles.add(new Vehicle(Direction.NORTH, 99060186, 5));
+        vehicles.add(new Vehicle(Direction.NORTH, 560601863, 5));
+        vehicles.add(new Vehicle(Direction.SOUTH, 776356956, 5));
+        int countOfVehiclesMovingTowardsSouth = helper.getVehicleCountByDirection(vehicles, Direction.SOUTH);
+        Assert.assertEquals(1, countOfVehiclesMovingTowardsSouth);
+    }
+
+    @Test
+    public void shouldReturnCountOfVehiclesForGivenVehicleDataWithDirectionNorth() {
+        vehicles.add(new Vehicle(Direction.NORTH, 99060186, 5));
+        vehicles.add(new Vehicle(Direction.NORTH, 560601863, 5));
+        vehicles.add(new Vehicle(Direction.SOUTH, 776356956, 5));
+        int countOfVehiclesMovingTowardsSouth = helper.getVehicleCountByDirection(vehicles, Direction.NORTH);
+        Assert.assertEquals(2, countOfVehiclesMovingTowardsSouth);
     }
 }
