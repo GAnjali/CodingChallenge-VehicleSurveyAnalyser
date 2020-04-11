79adb08dacc56a9c83f5c8013c312b293dad3373
ganjali <GAnjali@github.com>
2020-04-11 15:41:47 +0530

    Refactor VehicleCountReportGenerator

--- src/main/java/constants/VehicleSurveyAnalyserConstants.java
+++ src/main/java/constants/VehicleSurveyAnalyserConstants.java
@@ -8,4 +8,8 @@ public class VehicleSurveyAnalyserConstants {
     public static final String INVALID_DATA_MESSAGE = "Invalid Data found, Please provide proper data";
     public static final String INVALID_TIME_MESSAGE = "Invalid Time found, Please provide proper data";
     public static final String AVERAGE_WHEEL_BASE = "2.5f";
+    public static final String VEHICLE_COUNT_REPORT_GENERATOR_HEADING = "***************Vehicle Count Report***************";
+    public static final String TOTAL_VEHICLE_COUNT = "\t\t\tTotal vehicle count: ";
+    public static final String NORTH_VEHICLES_COUNT = "\t\t\ttotal vehicle count moving in North direction: ";
+    public static final String SOUTH_VEHICLES_COUNT = "\t\t\ttotal vehicle count moving in South direction: ";
 }
--- src/main/java/reportGenerator/VehicleCountReportGenerator.java
+++ src/main/java/reportGenerator/VehicleCountReportGenerator.java
@@ -2,21 +2,22 @@ package reportGenerator;
 
 import model.Direction;
 import model.Vehicle;
-import reportGenerator.timeperiod.TimePeriod;
 
 import java.util.List;
+
+import static constants.VehicleSurveyAnalyserConstants.*;
 
 public class VehicleCountReportGenerator implements DayWiseReportGenerator {
 
     @Override
     public void generate(List<Vehicle> vehicles) {
-        output.print("***************Vehicle Count Report***************");
+        output.print(VEHICLE_COUNT_REPORT_GENERATOR_HEADING);
         getReport(vehicles);
     }
 
     public void formatReport(List<Vehicle> vehicles) {
-        output.print("\t\t\tTotal vehicle count: " + vehicles.size());
-        output.print("\t\t\ttotal vehicle count moving in North direction: " + helper.getVehicleCountByDirection(vehicles, Direction.NORTH));
-        output.print("\t\t\ttotal vehicle count moving in South direction: " + helper.getVehicleCountByDirection(vehicles, Direction.SOUTH));
+        output.print(TOTAL_VEHICLE_COUNT + vehicles.size());
+        output.print(NORTH_VEHICLES_COUNT + helper.getVehicleCountByDirection(vehicles, Direction.NORTH));
+        output.print(SOUTH_VEHICLES_COUNT + helper.getVehicleCountByDirection(vehicles, Direction.SOUTH));
     }
 }
