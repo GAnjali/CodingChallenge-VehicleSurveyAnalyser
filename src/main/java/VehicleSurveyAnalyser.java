a8a7e074ce505ba4165c6571a4400f996ecf786d
ganjali <GAnjali@github.com>
2020-04-12 12:58:46 +0530

    Implement report generator intialization and generatre report

--- src/main/java/VehicleSurveyAnalyser.java
+++ src/main/java/VehicleSurveyAnalyser.java
@@ -1,15 +1,33 @@
-import IO.Input;
+import IO.input.Input;
+import counter.VehicleCounter;
+import exceptions.InvalidDataException;
+import exceptions.InvalidTimeException;
 import exceptions.NoSuchFileFoundException;
+import model.Vehicle;
+import reportGenerator.ReportGenerator;
+import reportGenerator.daywise.InterVehicularDistanceReportGenerator;
+import reportGenerator.daywise.PeakVolumeTimesReportGenerator;
+import reportGenerator.daywise.VehicleCountReportGenerator;
+import reportGenerator.distribution.SpeedDistributionReportGenerator;
 
 import java.io.IOException;
+import java.util.List;
 
 public class VehicleSurveyAnalyser {
-    public static void main(String[] args) throws IOException, NoSuchFileFoundException {
+    static List<String> records;
+
+    public static void main(String[] args) throws IOException, NoSuchFileFoundException, InvalidDataException, InvalidTimeException {
         initialize();
+        VehicleCounter vehicleCounter = new VehicleCounter();
+        List<Vehicle> vehicles = vehicleCounter.getVehicles(records);
+        ReportGenerator[] reportGenerator = {new VehicleCountReportGenerator(), new PeakVolumeTimesReportGenerator(), new SpeedDistributionReportGenerator(), new InterVehicularDistanceReportGenerator()};
+        for (ReportGenerator generator : reportGenerator) {
+            generator.generate(vehicles);
+        }
     }
 
     private static void initialize() throws IOException, NoSuchFileFoundException {
         Input input = new Input();
-        input.loadFile();
+        records = input.loadData();
     }
 }
--- src/main/java/constants/VehicleSurveyAnalyserConstants.java
+++ src/main/java/constants/VehicleSurveyAnalyserConstants.java
@@ -1,7 +1,7 @@
 package constants;
 
 public class VehicleSurveyAnalyserConstants {
-    public static final String DATA_FILE = System.getProperty("user.dir") + "/src/main/java/resources/Vehicle Survey Coding Challenge sample data.txt";
+    public static final String DATA_FILE = System.getProperty("user.dir") + "/src/main/resources/Vehicle Survey Coding Challenge sample data.txt";
     public static final String SENSOR1_NAME = "A";
     public static final String SENSOR2_NAME = "B";
     public static final String NO_FILE_FOUND_MESSAGE = "No Such File found, Please provide data file";
